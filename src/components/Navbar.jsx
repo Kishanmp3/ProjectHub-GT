f4ce3d648dce35fed4f47e0f41c0decc9187ab97
idke64 <deliciousbrain42@gmail.com>
2024-09-23 15:36:39 -0400

    login page and tailwind styles

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+const Navbar = ({ placeholder }) => {
+    return (
+        <nav className="w-full h-16 bg-surface-100 fixed">
+            <div className="nav-margins flex justify-between items-center h-full">
+                <div className="flex gap-x-14 items-center">
+                    <div className="font-semibold text-base">Project Hub @ GT</div>
+                    <div className="flex items-center gap-x-8">
+                        <button className="navlink">Navlink 1</button>
+                        <button className="navlink">Navlink 1</button>
+                        <button className="navlink">Navlink 1</button>
+                    </div>
+                    
+                </div>
+                
+                <div className="flex gap-x-6 items-center">
+                    <button className="secondary-btn">Sign In</button>
+                    <button className="primary-btn">Login</button>
+                    
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
