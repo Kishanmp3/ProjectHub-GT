f376f14796ad24ff35efa209ac87e0a4f5ee3650
VedZ <zved2003@gmail.com>
2024-09-24 00:25:51 -0400

    routed login button to login page

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Navbar from "./components/Navbar";
 
 const AppRouter = () => {
     return (
+        <>
         <Router>
+            <Navbar/>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
             </Routes>
         </Router>
+        </>
     )
 }
 
