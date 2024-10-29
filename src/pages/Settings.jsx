82b8c3bed23a4c6e038c4ecd66d4995eaff7881c
Mark Mogos <markmogos@gmail.com>
2024-10-29 18:19:04 -0400

    Added checkboxes and such

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -6,23 +6,53 @@ import Navbar from "../components/Navbar.jsx";
 const Settings = () => {
     const [selectedSection, setSelectedSection] = useState('profile');
 
+    const [isChecked, setIsChecked] = useState(false);
+    const [textValue, setTextValue] = useState('');
+
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+    };
+
+    const handleTextChange = (e) => {
+        setTextValue(e.target.value);
+    };
+
     const renderContent = () => {
         switch (selectedSection) {
             case 'profile':
                 return (
-                    <div className="settings-box">Profile Settings</div>
+                    <div className="settings-box">
+                        <label className="flex items-center space-x-3 mb-4">
+                            <input
+                                type="checkbox"
+                                checked={isChecked}
+                                onChange={handleCheckboxChange}
+                                className="form-checkbox h-5 w-5 text-primary-100"
+                            />
+                            <span>Enable Something</span>
+                        </label>
+                        <label className="flex flex-col space-y-2">
+                            <span>Change the field of something else:</span>
+                            <input
+                                type="text"
+                                value={textValue}
+                                onChange={handleTextChange}
+                                className="form-input mt-1 block w-f ull p-2 border border-surface-300 rounded text-black"
+                            />
+                        </label>
+                    </div>
                 );
             case 'account':
                 return (
-                    <div>Account Settings</div>
+                    <div className="settings-box">Account Settings</div>
                 );
             case 'notifications':
                 return (
-                    <div>Notification Settings</div>
+                    <div className="settings-box">Notification Settings</div>
                 );
             case 'privacy':
                 return (
-                    <div>Privacy Settings</div>
+                    <div className="settings-box">Privacy Settings</div>
                 );
             default:
                 return (
@@ -33,13 +63,13 @@ const Settings = () => {
 
     return (
         <div className="flex h-screen pt-16 overflow-hidden">
-                <img src={wave} className="fixed w-full h-full object-cover z-[-1]" alt="Background Wave"/>
+            <img src={wave} className="fixed w-full h-full object-cover z-[-1]" alt="Background Wave"/>
             <div className="fixed h-[calc(100vh-4rem)]">
                 <SettingsSidebar onSelect={setSelectedSection}/>
             </div>
-                <div className="flex-grow p-4 ml-64">
-                    {renderContent()}
-                </div>
+            <div className="flex-grow p-4 ml-64">
+                {renderContent()}
+            </div>
         </div>
     );
 };
